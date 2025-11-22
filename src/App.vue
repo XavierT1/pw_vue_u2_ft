ad7ac803ce4a1447c680d71d7c08d52129fcee24
Xavier Tapia <fxtapiar@uce.edu.ec>
2025-11-21 19:44:33 -0500

    Taller 13
    Componentes Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hola mundo desde Vue.js"/>
+  <div>
+    <ContadorView></ContadorView>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContadorView from '../src/views/ContadorView.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ContadorView,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Universidad Central del Ecuador</h1>
+    <h2>Carrera Computacion</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
